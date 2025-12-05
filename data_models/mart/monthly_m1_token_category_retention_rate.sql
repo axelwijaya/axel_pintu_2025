64f6c9eb15b6a27144d711a2c3d2c4fdeac87542
axelwijaya <axelwijaya22@gmail.com>
2025-12-06 00:45:44 +0700

    minor fix + data lineage pngs

--- data_models/mart/monthly_m1_token_category_retention_rate.sql
+++ data_models/mart/monthly_m1_token_category_retention_rate.sql
@@ -1,15 +1,17 @@
 with transacting_users as (
 	select date_trunc('month', fe.event_utc_datetime) as month
-		, fe.token_id
+		, dt.token_category
 		, fe.user_id
 		, max(fe.updated_utc_datetime) as updated_utc_datetime
 	from core.fact_events fe
+    left join core.dim_tokens dt
+        on fe.token_id = dt.token_id
 	where fe.event_status in ('SUCCESS', 'FILLED')
 	group by 1,2,3
 )
 , base as (
 	select curr.month
-		, curr.token_id
+		, curr.token_category
 		, curr.user_id as cm_user_id
 		, next.user_id as m1_retained_user_id
 		, greatest(curr.updated_utc_datetime, next.updated_utc_datetime) as updated_utc_datetime
@@ -17,10 +19,10 @@ with transacting_users as (
 	left join transacting_users next
 		on curr.month = next.month - interval '1 month'
 		and curr.user_id = next.user_id
-		and curr.token_id = next.token_id
+		and curr.token_category = next.token_category
 )
 select month
-	, token_id
+	, token_category
 	, count(cm_user_id) as current_month_user_count
 	, count(m1_retained_user_id)*1.0/current_month_user_count as m1_token_retention_rate
 	, max(updated_utc_datetime) as updated_utc_datetime
